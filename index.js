eca1d2c3180bf40562fd87746a68028e1740c909
Stephen Harvey <itsteveharvey@gmail.com>
2022-09-29 16:42:15 +1300

    Linked individual keypresses by putting switch inside makeSound function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Detects Button Press
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
@@ -7,48 +8,59 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
+    makeSound(buttonInnerHTML);
 
-      case "a":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-
-      case "s":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-      case "d":
-      var tom2 = new Audio ("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-      case "j":
-      var kick = new Audio ("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-
-      case "k":
-      var snare = new Audio ("sounds/snare.mp3");
-      snare.play();
-      break;
-
-      case "l":
-      var crash = new Audio ("sounds/crash.mp3");
-      crash.play();
-      break;
-
-
-      default:
-    }
   });
+
 }
 
-document.addEventListener("keypress", function () {
-  alert("Key was pressed!");
+// Detects Keyboard Press
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
 });
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+    var tom3 = new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+
+    case "a":
+    var tom4 = new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+
+    case "s":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+
+    case "d":
+    var tom2 = new Audio ("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+
+    case "j":
+    var kick = new Audio ("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+    case "k":
+    var snare = new Audio ("sounds/snare.mp3");
+    snare.play();
+    break;
+
+    case "l":
+    var crash = new Audio ("sounds/crash.mp3");
+    crash.play();
+    break;
+
+    default: console.log(key);
+
+  }
+}
